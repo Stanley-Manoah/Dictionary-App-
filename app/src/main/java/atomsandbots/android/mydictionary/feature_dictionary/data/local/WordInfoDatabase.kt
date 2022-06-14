51a7063dfac7fe82b0c8f7800995090adfc6d9e4
Your-Full-Name <your-email-address>
2022-06-14 11:54:58 +0100

    Database Version Upgrade (Version 2)

--- app/src/main/java/atomsandbots/android/mydictionary/feature_dictionary/data/local/WordInfoDatabase.kt
+++ app/src/main/java/atomsandbots/android/mydictionary/feature_dictionary/data/local/WordInfoDatabase.kt
@@ -1,13 +1,17 @@
 package atomsandbots.android.mydictionary.feature_dictionary.data.local
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import atomsandbots.android.mydictionary.feature_dictionary.data.local.entity.WordInfoEntity
 
 @Database(
+    version = 2,
     entities = [WordInfoEntity::class],
-    version = 2
+    autoMigrations = [
+        AutoMigration (from = 1, to = 2)
+    ],exportSchema = true
 )
 @TypeConverters(Converters::class)
 abstract class WordInfoDatabase: RoomDatabase() {
